cff54a287e38a7504e0987050826daa5c75ab947
Ernests Cers <ernests.cers@atea.com>
2025-05-26 16:15:07 +0300

    lesson 19. Need to finish the rendering if the lists

--- TemplateWebpart1-20/src/webparts/tempalte120/components/ITempalte120Props.ts
+++ TemplateWebpart1-20/src/webparts/tempalte120/components/ITempalte120Props.ts
@@ -1,7 +1,10 @@
+import { ISharePointList } from "../Tempalte120WebPart";
+
 export interface ITempalte120Props {
   description: string;
   isDarkTheme: boolean;
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+  lists: ISharePointList[]; // Add this line
 }
