7882c1261ad9735ec5c506a413739a609ea3dd6a
Ernests Cers <ernests.cers@atea.com>
2025-07-17 14:15:46 +0300

    69 Learning vid

---
+++ GraphAPIDemo/src/webparts/graphApiDemo/GraphApiDemoWebPart.ts
@@ -0,0 +1,136 @@
+import { Version } from "@microsoft/sp-core-library";
+import {
+  type IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import type { IReadonlyTheme } from "@microsoft/sp-component-base";
+//import { escape } from "@microsoft/sp-lodash-subset";
+
+//import styles from "./GraphApiDemoWebPart.module.scss";
+import * as strings from "GraphApiDemoWebPartStrings";
+
+//import { MSGraphClient } from "@microsoft/sp-http";
+import * as MicrosoftGraph from "@microsoft/microsoft-graph-types";
+
+export interface IGraphApiDemoWebPartProps {
+  description: string;
+}
+
+export default class GraphApiDemoWebPart extends BaseClientSideWebPart<IGraphApiDemoWebPartProps> {
+  //private _isDarkTheme: boolean = false;
+  //private _environmentMessage: string = "";
+
+  public render(): void {
+    this.context.msGraphClientFactory
+      .getClient("3")
+      .then((client: any): void => {
+        client
+          .api("/me")
+          .get((error: any, user: MicrosoftGraph.User, rawResponse?: any) => {
+            this.domElement.innerHTML = `
+    <div>
+
+    <p> Display Name: ${user.displayName}</p>
+    <p> Given Name: ${user.givenName}</p>
+    <p> Surname: ${user.surname}</p>
+    <p> Email ID: ${user.mail}</p>
+    <p> Mobile Phone: ${user.mobilePhone}</p>
+
+    </div>`;
+          });
+      });
+  }
+
+  // protected onInit(): Promise<void> {
+  //   return this._getEnvironmentMessage().then((message) => {
+  //     this._environmentMessage = message;
+  //   });
+  // }
+
+  // private _getEnvironmentMessage(): Promise<string> {
+  //   if (!!this.context.sdks.microsoftTeams) {
+  //     // running in Teams, office.com or Outlook
+  //     return this.context.sdks.microsoftTeams.teamsJs.app
+  //       .getContext()
+  //       .then((context) => {
+  //         let environmentMessage: string = "";
+  //         switch (context.app.host.name) {
+  //           case "Office": // running in Office
+  //             environmentMessage = this.context.isServedFromLocalhost
+  //               ? strings.AppLocalEnvironmentOffice
+  //               : strings.AppOfficeEnvironment;
+  //             break;
+  //           case "Outlook": // running in Outlook
+  //             environmentMessage = this.context.isServedFromLocalhost
+  //               ? strings.AppLocalEnvironmentOutlook
+  //               : strings.AppOutlookEnvironment;
+  //             break;
+  //           case "Teams": // running in Teams
+  //           case "TeamsModern":
+  //             environmentMessage = this.context.isServedFromLocalhost
+  //               ? strings.AppLocalEnvironmentTeams
+  //               : strings.AppTeamsTabEnvironment;
+  //             break;
+  //           default:
+  //             environmentMessage = strings.UnknownEnvironment;
+  //         }
+
+  //         return environmentMessage;
+  //       });
+  //   }
+
+  //   return Promise.resolve(
+  //     this.context.isServedFromLocalhost
+  //       ? strings.AppLocalEnvironmentSharePoint
+  //       : strings.AppSharePointEnvironment
+  //   );
+  // }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    // this._isDarkTheme = !!currentTheme.isInverted;
+    const { semanticColors } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty(
+        "--bodyText",
+        semanticColors.bodyText || null
+      );
+      this.domElement.style.setProperty("--link", semanticColors.link || null);
+      this.domElement.style.setProperty(
+        "--linkHovered",
+        semanticColors.linkHovered || null
+      );
+    }
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse("1.0");
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription,
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  }
+}
